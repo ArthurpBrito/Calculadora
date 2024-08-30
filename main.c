021676891c22fdacb61aaf1f4c26a825f6c2b8c9
Arthur Brito <132574394+ArthurpBrito@users.noreply.github.com>
2024-08-30 13:14:30 -0300

    Update main.c
    Adicionando a questão 2

--- main.c
+++ main.c
@@ -7,9 +7,8 @@ void binario(int n) {
 
     while (n > 0) {
         bin[i] = n % 2;
-        printf("%d (pois o resto de %d e = %d)\n", bin[i], n, bin[i]);
+        printf("%d (pois %d %% por 2 tem resto = %d)\n", bin[i], n, bin[i]);
         n = n / 2;
-        printf("Atualizando n: %d\n", n);
         i++;
     }
 
@@ -27,9 +26,8 @@ void octal(int n) {
 
     while (n > 0) {
         oct[i] = n % 8;
-        printf("%d (pois %d dividido por 8 e = %d)\n", oct[i], n, oct[i]);
+        printf("%d (pois %d %% por 8 = %d)\n", oct[i], n, oct[i]);
         n = n / 8;
-        printf("Atualizando n: %d\n", n);
         i++;
     }
 
@@ -52,9 +50,8 @@ void hexa(int n) {
         } else {
             hex[i] = 55 + resto;
         }
-        printf("%c (pois %d dividido por 16 e = %d)\n", hex[i], n, resto);
+        printf("%c (pois %d %% por 16 = %d)\n", hex[i], n, resto);
         n = n / 16;
-        printf("Atualizando n: %d\n", n);
         i++;
     }
 
@@ -71,7 +68,7 @@ void bcd(int n) {
 
     while (n > 0) {
         digito = n % 10;
-        printf("%d (pois %d dividido por 10  e = %d)\n", digito, n, digito);
+        printf("%d (pois %d %% por 10 = %d)\n", digito, n, digito);
         printf("Representacao em BCD de %d: ", digito);
         for (int i = 3; i >= 0; i--) {
             printf("%d", (digito >> i) & 1);
@@ -82,17 +79,77 @@ void bcd(int n) {
     printf("\n");
 }
 
-int main() {
-    int num;
+void complementoDois(int n) {
+    printf("Convertendo %d para complemento a 2 com 16 bits:\n", n);
 
-    printf("digitoe um numero decimal: ");
-    scanf("%d", &num);
+    unsigned short binario[16];
+    unsigned short num = (unsigned short)n;
+    int i = 0;
+
+    for (i = 0; i < 16; i++) {
+        binario[i] = (num >> i) & 1;
+        printf("%d (pois %d %% por 2^%d = %d)\n", binario[i], num, i, binario[i]);
+    }
+
+    printf("Binario (antes do complemento, se aplicavel): ");
+    for (int j = 15; j >= 0; j--) {
+        printf("%d", binario[j]);
+    }
+    printf("\n");
+
+    if (n < 0) {
+        printf("Como o numero e negativo, calculamos o complemento a 2:\n");
+        for (i = 0; i < 16; i++) {
+            binario[i] = ~binario[i] & 1;
+            printf("Inversao do bit %d: %d\n", i, binario[i]);
+        }
+
+        printf("Adicionando 1 ao binario invertido:\n");
+        for (i = 0; i < 16; i++) {
+            if (binario[i] == 0) {
+                binario[i] = 1;
+                break;
+            } else {
+                binario[i] = 0;
+            }
+        }
+
+        printf("Resultado final em complemento a 2: ");
+        for (int j = 15; j >= 0; j--) {
+            printf("%d", binario[j]);
+        }
+        printf("\n");
+    }
 
     printf("\n");
-    binario(num);
-    octal(num);
-    hexa(num);
-    bcd(num);
+}
+
+int main() {
+    int num;
+    int opcao;
+
+    printf("Escolha uma opcao:\n");
+    printf("1. Converter de base 10 para base 2, base 8, base 16 e BCD\n");
+    printf("2. Converter de base 10 para base com sinal (complemento a 2) com 16 bits\n");
+    printf("Digite a sua opcao: ");
+    scanf("%d", &opcao);
+
+    if (opcao == 1) {
+        printf("Digite um numero decimal: ");
+        scanf("%d", &num);
+        printf("\n");
+        binario(num);
+        octal(num);
+        hexa(num);
+        bcd(num);
+    } else if (opcao == 2) {
+        printf("Digite um numero decimal: ");
+        scanf("%d", &num);
+        printf("\n");
+        complementoDois(num);
+    } else {
+        printf("Opcao invalida!\n");
+    }
 
     return 0;
 }
